f6360053b063cef3c6f3f105ec002b91eeed8918
Kasim Hussain <kasiimh1@gmail.com>
2019-09-04 23:08:42 +0100

    Finally works! (:

--- SaveMe.py
+++ SaveMe.py
@@ -1,49 +1,65 @@
 import subprocess
 import os
+import sys
 
-print("Connect Device To Start..\n")
-igetNonceArgs = ("../igetnonce")
+import sys, os
+frozen = 'not'
+if getattr(sys, 'frozen', False):
+    # we are running in a bundle
+    frozen = 'ever so'
+    bundle_dir = sys._MEIPASS
+else:
+    # we are running in a normal Python environment
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
 
-popen = subprocess.Popen(igetNonceArgs, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+savePath = '~/Desktop'
+
+os.chdir(bundle_dir)
+print('Connect Device To Start..\n')
+
+os.chdir(os.getcwd() + '/SupportFiles/')
+
+popen = subprocess.Popen('./igetnonce', stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
 igetNonceOuput, stdErrValue = communicateRes
-igetNonceOuput = igetNonceOuput.split("\n")
+igetNonceOuput = igetNonceOuput.split('\n')
 
-ecid = ""
-boardid = ""
-deviceid = ""
-apnonce = ""
+ecid = ''
+boardid = ''
+deviceid = ''
+apnonce = ''
 iosversion = 0
 
 for s in igetNonceOuput:
-    if s.startswith("ECID="):
+    if s.startswith('ECID='):
         ecid = s
-        ecid = ecid.replace("ECID=","")
+        ecid = ecid.replace('ECID=','')
         ecid = ecid[:13]
 
-    if s.startswith("ApNonce="):
+    if s.startswith('ApNonce='):
         apnonce = s
-        apnonce = apnonce.replace("ApNonce=","")
+        apnonce = apnonce.replace('ApNonce=','')
 
-    if s.startswith("Identified device as "):
+    if s.startswith('Identified device as '):
         boardid = s
-        boardid = boardid.replace("Identified device as ","")
-        boardid = boardid.replace("in normal mode...","")
+        boardid = boardid.replace('Identified device as ','')
+        boardid = boardid.replace('in normal mode...','')
         deviceid = boardid[8:]
         boardid = boardid[:6]
         
-print("Device Model = " + deviceid)
-print("BoardConfig = " + boardid)
-print("ECID = " + ecid)
-print("ApNonce = " + apnonce)
+print('Device Model = ' + deviceid)
+print('BoardConfig = ' + boardid)
+print('ECID = ' + ecid)
+print('ApNonce = ' + apnonce)
+print('Enter the iOS version to save the ticket for')
 
-print("Enter the iOS version to save the ticket for")
-iosversion = raw_input()
+iosversion = input()
 
-tsscheckerArgs = '../tsschecker -d %s' %deviceid + ' -e %s' %ecid + ' --boardconfig %s' % (boardid) + ' --ios %s' % (iosversion) + ' --apnonce %s' % (apnonce) + ' -s --save-path ' + (os.getcwd()) + '/'
+tsscheckerArgs = './tsschecker -d %s' %deviceid + ' -e %s' %ecid + ' --boardconfig %s' % (boardid) + ' --ios %s' % (iosversion) + ' --apnonce %s' % (apnonce) + ' -s --save-path ' + (savePath)
 
-popen = subprocess.Popen([tsscheckerArgs], shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+popen = subprocess.Popen([tsscheckerArgs], shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
 tsscheckerOutput, stdErrValue = communicateRes
 
-print(tsscheckerOutput)
+print('File saved at path ' + savePath)
+print('Exiting')
