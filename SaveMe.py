3069a33e8d9e8ff1cba0d41205af95f5fd6a8ea1
Kasim Hussain <kasiimh1@gmail.com>
2019-09-06 23:07:00 +0100

    Added ticket checking and other fixes

--- SaveMe.py
+++ SaveMe.py
@@ -1,7 +1,10 @@
 import subprocess
 import os
 import sys
-import time
+import glob
+
+#requires
+#brew install --HEAD usbmuxd brew install --HEAD libimobiledevice brew install --HEAD ideviceinstaller
 
 frozen = 'not'
 if getattr(sys, 'frozen', False):
@@ -19,11 +22,28 @@ deviceid = ''
 apnonce = ''
 iosversion = 0
 
-savePath = '~/Desktop'
+hashes = '27325c8258be46e69d9ee57fa9a8fbc28b873df434e5e702a8b27999551138ae','3a88b7c3802f2f0510abc432104a15ebd8bd7154' ,'15400076bc4c35a7c8caefdcae5bda69c140a11bce870548f0862aac28c194cc','603be133ff0bdfa0f83f21e74191cf6770ea43bb'
+
+savePath = os.getcwd()
+
+if (os.path.isdir(savePath + '/SaveMe') is False):
+    os.mkdir(savePath + '/SaveMe')
+else:
+    print('\n\n[*] Skipping creating SaveMe folder as it already exists')
+
+savePath = savePath + '/SaveMe'
 
 os.chdir(bundle_dir)
-print('\n\n[*] Connect Device To Start..')
+
+print('[*] Connect Device To Start..')
+input('[*] Press ENTER when Device is connected > ')
 os.chdir(os.getcwd() + '/SupportFiles/')
+
+popen = subprocess.Popen('./ideviceinfo | grep UniqueChipID: ', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
+communicateRes = popen.communicate()
+ecid, stdErrValue = communicateRes
+ecid = ecid.strip()
+ecid = ecid[15:]
 
 popen = subprocess.Popen('./ideviceinfo | grep UniqueDeviceID: ', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
@@ -31,11 +51,11 @@ udid, stdErrValue = communicateRes
 udid = udid.strip()
 udid = udid[16:]
 
-popen = subprocess.Popen('./ideviceinfo | grep UniqueChipID: ', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
+popen = subprocess.Popen('./ideviceinfo | grep HardwarePlatform: ', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
-ecid, stdErrValue = communicateRes
-ecid = ecid.strip()
-ecid = ecid[15:]
+platform, stdErrValue = communicateRes
+platform = platform.strip()
+platform = platform[18:]
 
 popen = subprocess.Popen('./ideviceinfo | grep ProductType: ', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
@@ -52,33 +72,76 @@ boardid = boardid[15:]
 popen = subprocess.Popen('./ideviceenterrecovery %s' %udid, shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
 enterRecovery, stdErrValue = communicateRes
-print('[*] ' + enterRecovery)
 
-time.sleep(20) #some devices reboot faster than others
+print('[*] ' + enterRecovery.strip())
+input('[*] Press ENTER when Device is in Recovery Mode > ')
 
-popen = subprocess.Popen('./irecovery -q', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
+popen = subprocess.Popen('./igetnonce', stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 communicateRes = popen.communicate()
-apNonce, stdErrValue = communicateRes
-apNonce = apNonce.split('\n')
+igetNonceOuput, stdErrValue = communicateRes
+igetNonceOuput = igetNonceOuput.split('\n')
 
-for s in apNonce:
-    if s.startswith('NONC:'):
+for s in igetNonceOuput:
+    if s.startswith('ApNonce='):
         apnonce = s
-        apnonce = apnonce.replace('NONC:','')
+        apnonce = apnonce.replace('ApNonce=','')
+
+if (platform.find('t8020') != -1 or platform.find('t8027') != -1):
+    print('[*] Detected A12(X) Device')
+
+else:
+    for x in hashes:
+        if (apnonce == x) is True:
+            print('[*] Detected Non A12(X) Device')
+            print('[*] Device ApNonce matches the default unc0ver or chimera generator')
+            break;
         
+        else:
+            print('[*] Detected Non A12(X) Device')
+            print('[*] Device ApNonce does not match the unc0ver or chimera generator')
+            break;
+
 print('[D] Device Model = ' + deviceid)
 print('[D] Device UDID = ' + udid)
 print('[D] BoardConfig = ' + boardid)
 print('[D] ECID = ' + ecid)
 print('[D] ApNonce = ' + apnonce)
-print('[D] Enter the iOS version to save the ticket for')
 
-iosversion = input()
+iosversion = input('[D] Enter the iOS version to save the ticket for > ')
 
-tsscheckerArgs = './tsschecker -d %s' %deviceid + ' -e %s' %ecid + ' --boardconfig %s' %boardid + ' --ios %s' %iosversion + ' --apnonce %s' %apnonce + ' -s --save-path ' + savePath
+if (os.path.isdir(savePath + '/' + ecid) is False):
+    os.mkdir(savePath + '/' + ecid)
+else:
+    print('[*] Skipping creating ECID folder as %s it already exists' %ecid)
+savePath = savePath + '/' + ecid
 
+if (os.path.isdir(savePath + '/' + iosversion) is False):
+   os.mkdir(savePath + '/' + iosversion)
+else:
+    print('[*] Skipping creating iOS version folder as %s it already exists' %ecid)
+savePath = savePath + '/' + iosversion
+
+tsscheckerArgs = './tsschecker -d %s' %deviceid + ' -e %s' %ecid + ' --boardconfig %s' %boardid + ' --ios %s' %iosversion + ' --apnonce %s' %apnonce + ' -s --save-path %s' %savePath
 popen = subprocess.Popen([tsscheckerArgs], shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
-popen = subprocess.Popen('./irecovery -n', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
 
-print('[*] File saved at path ' + savePath)
+print('[*] Checking ticket against device in Recovery Mode')
+input('[*] Press Enter When File is Saved > ')
+
+ticket = ''
+for file in glob.glob(os.path.join(savePath, "*.shsh2")):
+    ticket = file
+
+print('[*] Checking this ticket ' + ticket)
+
+popen = subprocess.Popen('./futurerestore -w -t %s ' %ticket, shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
+communicateRes = popen.communicate()
+ticketCheck, stdErrValue = communicateRes
+print('\n\n[*] %s' %ticketCheck)
+print('[*] Exiting Recovery Mode')
+
+popen = subprocess.Popen('./futurerestore --exit-recovery', shell = True, stdout=subprocess.PIPE,stderr=subprocess.PIPE, encoding='utf8')
+if (ticket != None) is True:
+    print('[*] File Save as ' + ticket)
+else:
+    print('Problem occurred when saving SHSH ticket')
 print('[*] Exiting')
